23d3573aad9ef3d7ed3d0e43fd147f63dd399811
Zebraloew <mail@jakobmalek.com>
2022-01-13 17:25:01 +0100

    refinements. typography. automatic choices gen

--- tarot.py
+++ tarot.py
@@ -1,14 +1,10 @@
-from tarotlibrary import cards
 from tlibclass import deck
 import random
-
-# test
-#print(deck[1].give_meaning(True))
 
 ##1 select card
 ##2 check if upright
 ##3 print meaning
-##4 sublementing card
+##4 second card
 ##5 again?
 
 def tarot_round():
@@ -16,8 +12,12 @@ def tarot_round():
     print(15 * "\n")
 
     def choosing():
-        choice = input("Choose a card between 1 and 4: ")
-        choices = ["1","2","3","4"]
+        choices = []
+        for c in range(len(deck)):
+            choices.append(str(c+1))
+            print(str(c+1) + "  ", end="")
+        choice = input("\nChoose a card: \n")
+        
         if choice in choices:
                 return int(choice) - 1
         else:
@@ -26,37 +26,28 @@ def tarot_round():
 
     def upright_check():
         return random.choice([True,False])
-
-    def str_upright(bool):
-        if bool:
-            return ("reversed")
-        else:
-            return ("upright")
     
-    # choose a card
+    # user chooses a card
     choice = choosing()
 
-    ##1 select card
+    ##1 select random card
     selected = deck[random.randint(0,3)]
-    # prohibit that the same card show up twice
+    # prohibit that the same card shows up twice
     while selected == choice:
         selected = deck[random.randint(0,3)]
 
-    print(10*"\n" + "YOUR CARDS:\nThe " + selected[0], end=", ")
-
     ##2 check if upright
     orientation = upright_check()
-    print(str_upright(orientation))
 
     ##3 print meaning
-    print("\nINTERPRETATION\n" + selected[orientation+1])
+    print(10*"\n" + "YOUR CARDS:\nYour base card is:\n" + selected.give_meaning(orientation))
 
     ##4 Sublementing card
     sub_orientation = upright_check()
-    print("\nThis will be further elaborated by: \nThe " + cards[choice][0] + ", " + str_upright(sub_orientation) + "\n" + cards[choice][sub_orientation+1])
+    print("\nThis will be further elaborated by: \n" + deck[choice].give_meaning(sub_orientation))
 
     ##5 again?
-    repeat = input("Do you want to get another reading? (y/n) ")
+    repeat = input("\n\nDo you want to get another reading? (y/n)\n")
     if repeat == "y":
         tarot_round()
     else:
