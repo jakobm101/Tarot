275668bdbe8e7cd1fc04d888cd9fa954c6642bf4
Zebraloew <mail@jakobmalek.com>
2022-01-13 15:38:10 +0100

    working

--- tarot.py
+++ tarot.py
@@ -4,19 +4,55 @@ import random
 ##1 select card
 ##2 check if upright
 ##3 print meaning
+##4 sublementing card
+##5 again?
 
-#clear screen
-print(12*"\n")
+def tarot_round():
+    #clear screen
+    print(12 * "\n")
 
-##1 select card
-print(cards[random.randint(0,3)][0])
+    def choosing():
+        choice = input("Choose a card between 1 and 4: ")
+        choices = ["1","2","3","4"]
+        if choice in choices:
+                return int(choice)
+        else:
+            print("Wrong input.")
+            choosing()
 
-##2 check if upright
-reversed = random.choice([True,False])
-if reversed:
-    print("reversed")
-else:
-    print("upright")
+    def upright_check():
+        return random.choice([True,False])
 
-##3 print meaning
-print(2*"\n"+"")
+    def str_upright(bool):
+        if bool:
+            return ("reversed")
+        else:
+            return ("upright")
+
+    choice = choosing()
+
+    ##1 select card
+    selected = cards[random.randint(0,3)]
+    print(selected[0])
+
+    ##2 check if upright
+    orientation = upright_check()
+    print(str_upright(orientation))
+
+    ##3 print meaning
+    print("\nINTERPRETATION\n" + selected[orientation+1])
+
+    ##4 Sublementing card
+    sub_orientation = upright_check()
+    print("This will be further elaborated by: The " + cards[choice][0] + ", " + str_upright(sub_orientation) + "\n" + cards[choice][sub_orientation+1])
+
+    ##5 again?
+    repeat = input("Do you want to get aother reading? (y/n) ")
+    if repeat == "y":
+        tarot_round()
+    else:
+        print("OK, bye.")
+
+    
+
+tarot_round()
--- tarotlibrary.py
+++ tarotlibrary.py
@@ -1,8 +1,8 @@
 cards = [
-["Wizard","You are magical!","You see how things really are."],
-["World",""], 
-["Fool"], 
-["Lovers"]
+["Wizard", "You are magical!", "You see how things really are."],
+["World", "Assured success, recompense, voyage, route, emigration, flight, change of place.", "Inertia, fixity, stagnation, permanence."], 
+["Fool", "Folly, mania, extravagance, intoxication, delirium, frenzy, bewrayment.", "Negligence, absence, distribution, carelessness, apathy, nullity, vanity."], 
+["Lovers", "Attraction, love, beauty, trials overcome.", "Failure, foolish designs. Another account speaks of marriage frustrated and contrarieties of all kinds."]
 ]
 
 #print(cards[1])
